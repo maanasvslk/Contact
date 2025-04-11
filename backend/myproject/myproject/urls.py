f2b6c26dc8a42a742e2289578a8545bc883732a5
Maanas Krishna VSL <vslk.maanas@gmail.com>
2025-04-11 16:14:39 +0530

    Deploy

--- backend/myproject/myproject/urls.py
+++ backend/myproject/myproject/urls.py
@@ -4,7 +4,7 @@ from contact.views import version_redirect_view
 
 urlpatterns = [
     path('admin/', admin_site.urls),
-    path('', version_redirect_view('contact.urls')),
+    path('', version_redirect_view, name='home'),
     path('v2/', include('contact_v2.urls')),
     path('api/', include('contact.urls')),
     path('api/v2/', include('contact_v2.urls')),  # Corrected to match template
